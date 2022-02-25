dbaaafd6daef6f7c4e6b4e3749964920846d938f
siyazoff <siyazdarkhan@gmail.com>
2022-02-25 18:55:55 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+import { TodoState } from "./src/context/todo/TodoState";
+import { ScreenState } from "./src/context/screen/ScreenState";
+import { MainLayout } from "./src/MainLayout";
+
+async function loadApplication() {
+  await Font.loadAsync({
+    "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+}
+
+export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={loadApplication}
+        onError={(err) => console.log(err)}
+        onFinish={() => setIsReady(true)}
+      />
+    );
+  }
+
+  return (
+    <ScreenState>
+      <TodoState>
+        <MainLayout />
+      </TodoState>
+    </ScreenState>
+  );
+}
---
+++ src/screens/MainScreen.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, useContext } from "react";
+import { StyleSheet, View, FlatList, Image, Dimensions } from "react-native";
+import { AddTodo } from "../components/AddTodo";
+import { Todo } from "../components/Todo";
+import { THEME } from "../theme";
+import { TodoContext } from "../context/todo/todoContext";
+import { ScreenContext } from "../context/screen/screenContext";
+
+export const MainScreen = () => {
+  const { addTodo, todos, removeTodo } = useContext(TodoContext);
+  const { changeScreen } = useContext(ScreenContext);
+  const [deviceWidth, setDeviceWidth] = useState(
+    Dimensions.get("window").width - THEME.PADDING_HORIZONTAL * 2
+  );
+
+  useEffect(() => {
+    const update = () => {
+      const width =
+        Dimensions.get("window").width - THEME.PADDING_HORIZONTAL * 2;
+      setDeviceWidth(width);
+    };
+
+    const listener = Dimensions.addEventListener("change", update);
+
+    return () => {
+      listener.remove();
+    };
+  }, []);
+
+  let content = (
+    <View style={{ width: deviceWidth }}>
+      <FlatList
+        keyExtractor={(item) => item.id.toString()}
+        data={todos}
+        renderItem={({ item }) => (
+          <Todo todo={item} onRemove={removeTodo} onOpen={changeScreen} />
+        )}
+      />
+    </View>
+  );
+
+  if (todos.length === 0) {
+    content = (
+      <View style={styles.imgWrap}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/empty-man.png")}
+        />
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <AddTodo onSubmit={addTodo} />
+
+      {content}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  imgWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    height: 300,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+});
